950da0cdd1dde7698da07d2c5bc301e16bd69d4f
Olawole Olawale Martins <olawalemart@gmail.com>
2024-01-18 21:11:26 +0100

    my sql advanced

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -5,9 +5,10 @@ DELIMITER $$
 CREATE FUNCTION SafeDiv (a INT, b INT)
 RETURNS FLOAT DETERMINISTIC
 BEGIN
-    IF b = 0 THEN
-        RETURN 0;
-    END IF
-    RETURN a / b;
+    DECLARE result FLOAT DEFAULT 0;
+    IF b != 0 THEN
+        SET result = a / b;
+    END IF;
+    RETURN result;
 END $$
 DELIMITER ;
