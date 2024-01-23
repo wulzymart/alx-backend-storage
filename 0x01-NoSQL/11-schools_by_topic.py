5e90bdf8e184ac67fd1ac5ae0c4679edb6e29731
Olawole Olawale Martins <olawalemart@gmail.com>
2024-01-23 21:53:51 +0100

    Nosql mandatory

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,4 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """returns list of documents with topic"""
-    return mongo_collection.find({"topic": topic})
+    return mongo_collection.find({"topics": topic})
